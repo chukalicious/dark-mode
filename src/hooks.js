6235b352fed6de24e5ebb5934d71081e5fd01dc0
Katiuska <katiuskaalicea@gmail.com>
2020-11-05 20:26:56 -0800

    added the setValue function to my useLocalStorage hook and imported the hook to the Navbar component

--- src/hooks.js
+++ src/hooks.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 
-export const useLocalStorage = (key, value) => {
+export const useLocalStorage = (key, initialValue) => {
   const [storedValue, setStoredValue] = useState(() => {
     const item = localStorage.getItem(key);
-    return item ? JSON.parse(item) : value;
+    return item ? JSON.parse(item) : initialValue;
   });
 
-  return [storedValue];
+  const setValue = (value) => {
+    localStorage.setItem(key, JSON.stringify(value));
+  };
+  return [storedValue, setValue];
 };
